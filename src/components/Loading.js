fa7a0f92de48d43485b9687c25c163dce71bc462
Marcus Guimarães <mrenanrguimaraes@gmail.com>
2022-07-04 16:15:48 -0300

    Requisito 2 completo!

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Loading extends Component {
+  render() {
+    return (
+      <div className="loading">
+        <h1>Carregando...</h1>
+      </div>
+    );
+  }
+}
+export default Loading;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,60 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
-export default class Login extends Component {
+class Login extends Component {
+  state = {
+    name: '',
+    loading: false,
+    minLen: 3,
+    done: false,
+  }
+
+  handleLogin = () => {
+    const { name } = this.state;
+    this.setState({ loading: true },
+      async () => {
+        await createUser({ name });
+        this.setState({ loading: false, done: true });
+      });
+  }
+
   render() {
+    const {
+      name,
+      loading,
+      minLen,
+      done,
+    } = this.state;
+
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <form id="loginForm">
+          <input
+            type="text"
+            data-testid="login-name-input"
+            value={ name }
+            onChange={ (event) => this.setState({
+              name: event.target.value,
+            }) }
+          />
+
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            onClick={ this.handleLogin }
+            disabled={ name.length < minLen }
+          >
+            Login
+          </button>
+        </form>
+        { loading && <Loading /> }
+        { done && <Redirect to="/search" /> }
+      </div>
+
     );
   }
 }
+
+export default Login;
